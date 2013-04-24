8e21b033bf008414982382013480b41d4a188f59
Arnaud Courbiere <arnaud.courbiere@gmail.com>
2013-04-23 23:28:05 -0700

    Daemon: Handling pid file. Startup script: Adjusting daemon options.

--- main.c
+++ main.c
@@ -8,20 +8,53 @@
 #include <unistd.h>
 #include <syslog.h>
 #include <string.h>
+#include <signal.h>
+
+#define PID_DIR "/var/run/ping_monitor"
+#define PID_FILE "/var/run/ping_monitor/pid"
+
+static void handler(int sig, siginfo_t *siginfo, void *context)
+{
+    unlink(PID_FILE);
+    _exit(0);
+}
 
 int main(int argc, char *argv[])
 {
     void ping_log(char*, ...);
-    pid_t ppid, pid, sid;
-    char pid_dir[] = "/var/run/ping_monitor";
-    char pid_file[] = "/var/run/ping_monitor/pid";
+    //static void handler(int, siginfo_t*, void*);
 
-    mkdir(pid_dir, 0755);
+    // Setup signal handler
+    struct sigaction act;
+    memset(&act, '\0', sizeof(act));
+    act.sa_sigaction = &handler;
+    act.sa_flags = SA_SIGINFO;
+
+    if (sigaction(SIGTERM, &act, NULL) < 0) {
+        ping_log("sigaction: unable to register signal handler for SIGTERM.");
+        exit(EXIT_FAILURE);
+    }   
+    if (sigaction(SIGINT, &act, NULL) < 0) {
+        ping_log("sigaction: unable to register signal handler for SIGINT.");
+        exit(EXIT_FAILURE);
+    }   
+    if (sigaction(SIGHUP, &act, NULL) < 0) {
+        ping_log("sigaction: unable to register signal handler for SIGHUP.");
+        exit(EXIT_FAILURE);
+    }   
+
+    // Setup ping daemon
+    pid_t ppid, pid, sid;
+
+    if (mkdir(PID_DIR, 0755) == -1 && errno != EEXIST) {
+        printf("Unable to create pid directory %s.\n", PID_DIR);
+        exit(EXIT_FAILURE);
+    }
 
     ppid = getpid();
     printf("Parent pid is %d.\n", ppid);
 
-    // Fork off parent process.
+    // Fork off parent process
     printf("Forking child process...\n");
     pid = fork();
 
@@ -30,18 +63,19 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    // Exit the parent process.
+    // Exit the parent process
     if (pid > 0) {
-        FILE *fp = fopen(pid_file, "w");
+        FILE *fp = fopen(PID_FILE, "w");
 
         if (fp == NULL) {
-            printf("Failed to open file %s.", pid_file);
+            printf("Failed to open file %s.\n", PID_FILE);
+            exit(EXIT_FAILURE);
         }
 
         int len = fprintf(fp, "%d", pid);
 
         if (len < 0) {
-            printf("Failed to write pid to pid file.");
+            printf("Failed to write pid to pid file.\n");
             exit(EXIT_FAILURE);
         }
 
@@ -51,12 +85,12 @@ int main(int argc, char *argv[])
         exit(EXIT_SUCCESS);
     }
 
-    // Change the file mode mask.
+    // Change the file mode mask
     umask(0);
 
-    // Open any logs here.
+    // Open any logs here
 
-    // Create a new SID for the child process.
+    // Create a new SID for the child process
     sid = setsid();
 
     if (sid < 0) {
@@ -64,17 +98,18 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    // Change the cwd to root dir.
+    // Change the cwd to root dir
     if (chdir("/") < 0) {
         ping_log("Unable to change the CWD to root");
         exit(EXIT_FAILURE);
     }
 
-    // Close the standard file descriptors.
+    // Close the standard file descriptors
     close(STDIN_FILENO);
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
 
+    // Daemon code
     int online = 0;
     int success;
 
@@ -83,12 +118,12 @@ int main(int argc, char *argv[])
 
         if (success == 0) {
             if (online == 0) {
-                // Online.
+                // Online
                 online = 1;
                 ping_log("Online");
             }
         } else if (online == 1) {
-            // Offline.
+            // Offline
             online = 0;
             ping_log("Offline");
         }
@@ -111,3 +146,4 @@ void ping_log( char *buff,...)
 
     va_end(arglist);
 }
+
